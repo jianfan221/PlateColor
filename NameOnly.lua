402cad705976676175d02e8dae62805a21b05aa2
arciay <32655163@qq.com>
2026-05-22 10:12:28 +0800

    重写NPC名字模式,以解决可攻击血条偶尔不显示的问题
    - 重写NPC名字模式,以解决可攻击血条偶尔不显示的问题

--- NameOnly.lua
+++ NameOnly.lua
@@ -2,6 +2,7 @@ local _, ns = ...
 
 local function SetOnlyNameScale(self)
 	if not self.unit then return end
+	if not PlateColorDB.onlyName and not PlateColorDB.onlyNameNpc then return end
 	if self:IsForbidden() then
 		SystemFont_NamePlate:SetFont(SystemFont_NamePlate:GetFont(),1,"OUTLINE")
 		SystemFont_NamePlate:SetFont(SystemFont_NamePlate:GetFont(),PlateColorDB.helpNameScale,"OUTLINE")
@@ -15,33 +16,32 @@ ns.hook(NamePlateUnitFrameMixin, "OnUnitSet", function(self)
 end)
 
 local function TrySetOnlyName(self)
+	if not self then return end
 	if not self.unit then return end
 	if not PlateColorDB.onlyNameNpc then return end
-	if not self:IsPlayer() and (self:IsForbidden() or self:IsFriend()) then
+	if UnitCanAttack("player", self.unit) then
+		TextureLoadingGroupMixin.RemoveTexture({ textures = self }, "showOnlyName")
+		TextureLoadingGroupMixin.RemoveTexture({ textures = self.castBar }, "showOnlyName")
+		TextureLoadingGroupMixin.RemoveTexture({ textures = self.castBar }, "widgetsOnly")
+		TextureLoadingGroupMixin.RemoveTexture({ textures = self.HealthBarsContainer.healthBar }, "showOnlyName")
+		TextureLoadingGroupMixin.RemoveTexture({ textures = self.ClassificationFrame }, "showOnlyName")
+		if self.healthBar and self.healthBar.show then
+			self.healthBar:Hide()
+			self.healthBar:Show()
+		end
+		if self.castBar and self.castBar.show and (UnitCastingInfo(self.unit) ~= nil or UnitChannelInfo(self.unit) ~= nil) then
+			self.castBar:Show()
+		end
+	elseif not self:IsPlayer() and (self:IsForbidden() or not UnitCanAttack("player", self.unit)) then
 		TableUtil.TrySet(self, "showOnlyName")
 		TableUtil.TrySet(self.castBar, "showOnlyName")
 		TableUtil.TrySet(self.castBar, "widgetsOnly")
 		TableUtil.TrySet(self.HealthBarsContainer.healthBar, "showOnlyName")
 		TableUtil.TrySet(self.ClassificationFrame, "showOnlyName")
 		TableUtil.TrySet(self.optionTable, "colorNameBySelection")
-	elseif not self:IsFriend() then
-		TextureLoadingGroupMixin.RemoveTexture({ textures = self }, "showOnlyName")
-		TextureLoadingGroupMixin.RemoveTexture({ textures = self.castBar }, "showOnlyName")
-		TextureLoadingGroupMixin.RemoveTexture({ textures = self.castBar }, "widgetsOnly")
-		TextureLoadingGroupMixin.RemoveTexture({ textures = self.HealthBarsContainer.healthBar }, "showOnlyName")
-		TextureLoadingGroupMixin.RemoveTexture({ textures = self.ClassificationFrame }, "showOnlyName")
-		if self.HealthBarsContainer.healthBar and self.HealthBarsContainer.healthBar.show then
-			self.HealthBarsContainer.healthBar:Show()
-		end
-		if self.castBar and self.castBar.show and (UnitCastingInfo(self.unit) ~= nil or UnitChannelInfo(self.unit) ~= nil) then
-			self.castBar:Show()
-		end
 	end
 end
 
 ns.hook(NamePlateUnitFrameMixin, "UpdateShowOnlyName", function(self)
 	TrySetOnlyName(self)
-	C_Timer.After(0, function()
-		TrySetOnlyName(self)
-	end)
 end)
