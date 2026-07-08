616517f035d3dd8c815740ef97ec0ca8ad4cb857
arciay <32655163@qq.com>
2026-07-09 07:58:44 +0800

    驱散染色添加 12.1 版本防御

--- PlateAuras.lua
+++ PlateAuras.lua
@@ -13,37 +13,40 @@ local function SetCooldownText(self)
 end
 ns.hook(NamePlateAuraItemMixin,"OnLoad",SetCooldownText)
 
---驱散颜色
-local dispelColor = C_CurveUtil.CreateColorCurve()
-dispelColor:SetType(Enum.LuaCurveType.Step)
-dispelColor:AddPoint(0, CreateColor(0,  0,  0,  0))--无
-dispelColor:AddPoint(1, CreateColor(1,  1,  1,  1))--魔法
-dispelColor:AddPoint(2, CreateColor(0.5,0,  1,  1))--诅咒
-dispelColor:AddPoint(3, CreateColor(1,0.5,  0,  1))--疾病
-dispelColor:AddPoint(4, CreateColor(0,  1,  0,  1))--中毒
-dispelColor:AddPoint(9, CreateColor(1,  0,  0,  1))--激怒
-ns.hook(NamePlateAuraItemMixin, "SetAura", function(self,aura)
-    if self and not self:IsForbidden() then
-        self:EnableMouse(not PlateColorDB.hideAuraTooltip)
-    end
+--驱散颜色 (12.1 前使用旧版)
+local _, _, _, tocversion = GetBuildInfo()
+if tocversion < 120100 then
+    local dispelColor = C_CurveUtil.CreateColorCurve()
+    dispelColor:SetType(Enum.LuaCurveType.Step)
+    dispelColor:AddPoint(0, CreateColor(0,  0,  0,  0))--无
+    dispelColor:AddPoint(1, CreateColor(1,  1,  1,  1))--魔法
+    dispelColor:AddPoint(2, CreateColor(0.5,0,  1,  1))--诅咒
+    dispelColor:AddPoint(3, CreateColor(1,0.5,  0,  1))--疾病
+    dispelColor:AddPoint(4, CreateColor(0,  1,  0,  1))--中毒
+    dispelColor:AddPoint(9, CreateColor(1,  0,  0,  1))--激怒
+    ns.hook(NamePlateAuraItemMixin, "SetAura", function(self,aura)
+        if self and not self:IsForbidden() then
+            self:EnableMouse(not PlateColorDB.hideAuraTooltip)
+        end
 
-    if self and not self:IsForbidden() and self.unitToken then
-        if not self.Stealable then
-            self.Stealable = self:CreateTexture(nil, "OVERLAY")
-            self.Stealable:SetPoint("TOPLEFT", self, "TOPLEFT", -5, 5)
-            self.Stealable:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", 5, -5)
-            self.Stealable:SetTexture("Interface\\TargetingFrame\\UI-TargetingFrame-Stealable")
-            self.Stealable:SetBlendMode("ADD")
+        if self and not self:IsForbidden() and self.unitToken then
+            if not self.Stealable then
+                self.Stealable = self:CreateTexture(nil, "OVERLAY")
+                self.Stealable:SetPoint("TOPLEFT", self, "TOPLEFT", -5, 5)
+                self.Stealable:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", 5, -5)
+                self.Stealable:SetTexture("Interface\\TargetingFrame\\UI-TargetingFrame-Stealable")
+                self.Stealable:SetBlendMode("ADD")
+            end
+            self.Stealable:Hide()
+            local color = C_UnitAuras.GetAuraDispelTypeColor(self.unitToken, aura.auraInstanceID, dispelColor)
+            if color and UnitCanAttack("player", self.unitToken) then
+                self.Stealable:SetVertexColor(color:GetRGB())
+                self.Stealable:SetAlphaFromBoolean(self.isBuff,255,0)
+                self.Stealable:Show()
+            end
         end
-        self.Stealable:Hide()
-        local color = C_UnitAuras.GetAuraDispelTypeColor(self.unitToken, aura.auraInstanceID, dispelColor)
-        if color and UnitCanAttack("player", self.unitToken) then
-            self.Stealable:SetVertexColor(color:GetRGB())
-            self.Stealable:SetAlphaFromBoolean(self.isBuff,255,0)
-            self.Stealable:Show()
-        end
-    end
-end)
+    end)
+end
 
 function ns.CrowdControlListFrameScale(unitFrame)
     unitFrame.AurasFrame.DebuffListFrame:SetScale(PlateColorDB.auraTopScale)
